cd3908df5a98c692f0cd5ab9a25028cf87087a36
Hemant Kumar <hemantksingh.hk@gmail.com>
2016-07-11 23:54:05 +0100

    Move to .Net core 1.0

--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+
 using OdeToFood.Controllers;
 
 namespace OdeToFood
@@ -12,7 +13,6 @@ namespace OdeToFood
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvcCore();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -20,16 +20,11 @@ namespace OdeToFood
         {
             app.UseMiddleware<BasicAuthMiddleware>();
 
-            app.UseMvc(routeBuilder =>
-              routeBuilder.MapRoute("Default", "{controller}/{action}/{id?}"));
 
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
         }
-
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
