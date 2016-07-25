ed198a6cc8a7e966f4da4644403b8004065a22d1
hemantksingh <hemantksingh.hk@gmail.com>
2016-07-25 11:22:29 +0100

    Remove basic auth.

--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -18,9 +18,6 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseMiddleware<BasicAuthMiddleware>();
-
-
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
